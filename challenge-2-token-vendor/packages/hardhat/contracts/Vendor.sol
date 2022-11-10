d68c8e505b68c746ebb49dc2d0de797084385a92
Anubhav Singh <singhanubhav58@gmail.com>
2022-11-10 13:45:13 +0530

    challenge-4-dex

--- challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
+++ challenge-2-token-vendor/packages/hardhat/contracts/Vendor.sol
@@ -5,8 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
-    //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
+    
     YourToken public yourToken;
 
     uint256 public constant tokensPerEth = 100;
